ace532dd9a234c8ff9fe1e894c3128d18fb20339
Charles Sauvat <charles.sauvat@hotmail.com>
2021-11-15 10:31:19 +0100

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("Student.php");
+
+$student = new Student('Nicolas', 10);
+
+$page = file_get_contents("page.html");
+$page = str_replace('$prenom', $student->Prenom, $page);
+$page = str_replace('$age', $student->Age, $page);
+
+echo $page;
